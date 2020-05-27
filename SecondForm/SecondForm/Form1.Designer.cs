42fa88cee4548865ae33e48b1596b12aaf4fb662
ilya <beqasmile@gmail.com>
2020-05-27 19:08:32 +0300

    all

---
+++ SecondForm/SecondForm/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace SecondForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.Status = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Success";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(228, 30);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 58);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Fail";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Status
+            // 
+            this.Status.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Status.Location = new System.Drawing.Point(37, 129);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(350, 29);
+            this.Status.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 186);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.TextBox Status;
+    }
+}
+
---
+++ SecondForm/SecondForm/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SecondForm
+{
+    public partial class Form1 : Form
+    {
+
+        delegate void MyFirstDelegate();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyFirstDelegate mySuccessDelegate = new MyFirstDelegate(callMeOnSuccess);
+
+            mySuccessDelegate.Invoke(); 
+        }
+
+        private static void callMeOnSuccess()
+        {
+            MessageBox.Show("Success");
+        }
+
+
+        private static void callMeOnFail()
+        {
+            MessageBox.Show("fail");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MyFirstDelegate myFailDelegate = new MyFirstDelegate(callMeOnFail);
+            myFailDelegate.Invoke();
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Status.Text = "Mouse down";
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            Status.Text = "Mouse hover";
+        }
+    }
+}
